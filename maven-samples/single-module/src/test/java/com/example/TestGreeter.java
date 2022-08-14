b3d8fecb0bd43406d880f74c7aea4a122e09c143
Sunil <Sunil.ritm@gmail.com>
2022-08-14 12:01:10 +0530

    change in test code sunday

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
 
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessageSunday() {
     String someone = "World, Happy Weekend 14-Aug-2022";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
